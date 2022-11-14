9956e798edf01a69d75ed990dfd5dc2cf4099d63
ernanilima <ernani.tecc@gmail.com>
2022-11-14 00:29:52 -0300

    Docker, banner e perfil executor

--- producer/src/main/java/br/com/ernanilima/producer/ProducerApplication.java
+++ producer/src/main/java/br/com/ernanilima/producer/ProducerApplication.java
@@ -8,7 +8,6 @@ public class ProducerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
-		System.out.println("Aplicacao de pe");
 	}
 
 }
