1f5d942c80dbf3c12008a0baaa18e3e112e17ce6
ernanilima <ernani.tecc@gmail.com>
2022-11-17 22:53:20 -0300

    Criado consumer-email

---
+++ consumer-email/src/main/java/br/com/ernanilima/consumeremail/ConsumerEmailApplication.java
@@ -0,0 +1,13 @@
+package br.com.ernanilima.consumeremail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerEmailApplication.class, args);
+	}
+
+}
--- consumer-email/src/test/java/br/com/ernanilima/consumeremail/ConsumerEmailApplicationTests.java
+++ consumer-email/src/test/java/br/com/ernanilima/consumeremail/ConsumerEmailApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.ernanilima.producer;
+package br.com.ernanilima.consumeremail;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProducerApplicationTests {
+class ConsumerEmailApplicationTests {
 
 	@Test
 	void contextLoads() {
