0bd8bdbdb5319fbadfd47201c9eac012c325ca05
ernanilima <ernani.tecc@gmail.com>
2022-11-21 10:47:51 -0300

    Atualizado teste de integracao

--- producer/src/test/java/br/com/ernanilima/producer/ProducerTestIT.java
+++ producer/src/test/java/br/com/ernanilima/producer/ProducerTestIT.java
@@ -1,15 +1,26 @@
 package br.com.ernanilima.producer;
 
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@AutoConfigureWebMvc
+import static org.testcontainers.utility.DockerImageName.parse;
+
+@Testcontainers
 @AutoConfigureMockMvc
-@ImportAutoConfiguration
 @SpringBootTest(classes = {ProducerApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public abstract class ProducerTestIT {
 
+    @Container
+    public static KafkaContainer kafka = new KafkaContainer(parse("confluentinc/cp-kafka:latest"));
+
+    @DynamicPropertySource
+    static void registerKafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+    }
 }
 
--- producer/src/test/java/br/com/ernanilima/producer/config/ProducerConsumerConfig.java
+++ producer/src/test/java/br/com/ernanilima/producer/config/ProducerConsumerConfig.java
@@ -1,20 +1,15 @@
 package br.com.ernanilima.producer.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.junit.ClassRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.utility.DockerImageName;
 
 import java.io.Serializable;
 import java.util.HashMap;
@@ -23,25 +18,25 @@ import java.util.Map;
 @TestConfiguration
 public class ProducerConsumerConfig {
 
-    @ClassRule
-    public static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+    private final KafkaProperties kafkaProperties;
 
-    @Bean
-    public ProducerFactory<String, Serializable> producerFactory() {
-        Map<String, Object> configs = new HashMap<>();
-
-        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
-        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(configs, new StringSerializer(), new JsonSerializer<>());
+    @Autowired
+    public ProducerConsumerConfig(KafkaProperties kafkaProperties) {
+        this.kafkaProperties = kafkaProperties;
     }
 
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, Serializable> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Serializable> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Serializable> consumerFactory() {
         Map<String, Object> configs = new HashMap<>();
 
-        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka.getBootstrapServers());
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
